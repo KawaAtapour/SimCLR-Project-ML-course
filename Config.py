de5201ba1a993916f74635ccc6278b084f7f8efa
Kawa Atapour <kawaatapour@gmail.com>
2025-11-04 22:48:52 -0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -12,16 +12,18 @@ def get_args():
 
     parser.add_argument('--dataset',  type=str, default="cifar10")
     parser.add_argument('--num_train_samples', type=int, default=10000)
-    parser.add_argument('--num_test_samples', type=int, default=1000)
+    parser.add_argument('--num_test_samples', type=int, default=9000)
 
     parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--epochs', type=int, default=40)
+    parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--batch_size_eval', type=int, default=64)
-    parser.add_argument('--epochs_eval', type=int, default=40)
+    parser.add_argument('--epochs_eval', type=int, default=100)
 
 
-    parser.add_argument('--warmup_epochs', type=int, default=5)
+    parser.add_argument('--warmup_epochs', type=int, default=10)
     parser.add_argument('--debug', type=bool, default=False)
+    parser.add_argument('--saved_model', type=bool, default=True)
+    
 
 
     return parser.parse_args()
